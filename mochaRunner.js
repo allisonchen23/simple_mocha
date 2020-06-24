f9f148530268c6e9cb58556a77fb619ed8b1fe98
Allison Chen <allison.chen@oracle.com>
2020-06-24 11:45:12 -0500

    added await in mochaRunnner

--- mochaRunner.js
+++ mochaRunner.js
@@ -1,14 +1,25 @@
 var Mocha = require('mocha');
 fs = require('fs');
 path = require('path');
-const {argv} = require('yargs'); //n_warmup_iters, n_perf_iters
-console.log(argv.warmup_iters);
-console.log(argv.perf_iters);
 
+//simple yargs
+// const {argv} = require('yargs'); //n_warmup_iters, n_perf_iters
+// console.log(argv.warmup_iters);
+// console.log(argv.perf_iters);
+
+var argv = require('yargs')
+    .option('n_warmup_iter', {
+        alias: 'w',
+        describe: 'Number of warm up iterations'
+    })
+    .argv
+
+console.log(argv.w); //can also use argv.n_warmup_iter
 var mocha = new Mocha();
 var testDir = 'test';
 
-async function runMocha() {
+async function runMocha(wi) {
+    console.log("Number of warm up iterations: " + wi);
     fs.readdirSync(testDir).filter(function(file) {
         return file.substr(-3) ==='.js';
     }).forEach(function(file) {
@@ -21,11 +32,15 @@ async function runMocha() {
     return "fin";
 }
 
-var i;
-for (i=0; i<3; i++)
-{
-    // reinstantiate a new Mocha object, run it, then unload files to clear the cache
-    mocha = new Mocha();
-    runMocha();
-    mocha.unloadFiles();
+async function main() {
+    var i;
+    for (i=0; i<2; i++)
+    {
+        // reinstantiate a new Mocha object, run it, then unload files to clear the cache
+        mocha = new Mocha();
+        await runMocha();
+        await mocha.unloadFiles();
+    }
 }
+
+main();
--- test/testsum.js
+++ test/testsum.js
@@ -1,6 +1,27 @@
 var sum = require('../sum');
 var expect = require('chai').expect;
 
+var arr = [2,3]; //n_warmup_iter, n_perf_iter
+
+// for (var i = 0; i < 2; i++) {
+//     describe('#sum()', function () {
+//         context('without arguments', function () {
+//             it('should return 0', function() {
+//                 expect(sum()).to.equal(0);
+//             })
+//         })
+//         context('with number arguments', function() {
+//             it('should return sum of arguments', function() {
+//                 expect(sum(1,2,3,4,5)).to.equal(15);
+//             })
+    
+//             it('should return argument when only 1 argument is passed', function () {
+//                 expect(sum(5)).to.equal(5);
+//             })
+//         })
+//     })
+// }
+
 describe('#sum()', function () {
     context('without arguments', function () {
         it('should return 0', function() {
