2c00ea4353a2dd5a7bc02e3fdf01ea336bbe69d5
Allison Chen <allison.chen@oracle.com>
2020-06-26 09:26:26 -0500

    uses callbacks to adjust global variables

--- mochaRunner.js
+++ mochaRunner.js
@@ -1,4 +1,5 @@
 var Mocha = require('mocha');
+const { INSPECT_MAX_BYTES } = require('buffer');
 fs = require('fs');
 path = require('path');
 
@@ -13,34 +14,92 @@ var argv = require('yargs')
         describe: 'Number of warm up iterations'
     })
     .argv
-
+let itr = 0;
+async function callback(failures) {
+    // itr++;
+    i++;
+    running = false;
+    console.log("Iteration number " + (itr+1) + "\nNumber of failures: " + failures);
+    return Promise.resolve(itr++);
+}
 console.log(argv.w); //can also use argv.n_warmup_iter
 var mocha = new Mocha();
 var testDir = 'test';
 
+let sleep = function (time_ms) {
+    let promise = new Promise((resolve) => {
+        setTimeout(() => {resolve();}, time_ms);
+    });
+    return promise;
+}
 async function runMocha(wi) {
-    console.log("Number of warm up iterations: " + wi);
+    console.log("in runMocha()");
     fs.readdirSync(testDir).filter(function(file) {
         return file.substr(-3) ==='.js';
     }).forEach(function(file) {
         mocha.addFile(path.join(testDir,file));
     });
 
-    mocha.run(function(failures) {
-        process.exitCode = failures ? 1: 0;
+    // mocha.run(function(failures) {
+    //     process.exitCode = failures ? 1: 0;
+    // });
+    // mocha.run(async function(failures) {
+    //     itr++;
+    //     let promise = new Promise((resolve, failures) => {
+    //     resolve("Iteration number " + itr + "\nNumber of failures: " + failures);
+    // });
+
+    mocha.run(async function(failures) { 
+        console.log(await callback(failures));
     });
+    // return promise;
+    mocha.unloadFiles();
     return "fin";
 }
+let running = false;
+let i=0;
 
 async function main() {
-    var i;
-    for (i=0; i<2; i++)
-    {
-        // reinstantiate a new Mocha object, run it, then unload files to clear the cache
-        mocha = new Mocha();
-        await runMocha();
-        await mocha.unloadFiles();
+    // var i=0;
+    while (i < 2) {
+        if ( !running ) {
+            if (i === itr) {
+                running = true;
+                mocha = new Mocha();
+                console.log("pre mocha run");
+                await runMocha();
+            }
+            else {
+                i++;
+            }
+        }
+        else {
+            await sleep(1000);
+            console.log("i: " + i + "\nitr: " + itr);
+        }
+        // mocha = new Mocha();
+        // console.log("pre mocha run");
+        // await runMocha();
+        // // while (itr != i) {
+        // //     sleep(500);
+        // // }
+        // await sleep(5000);
+        // console.log("post");
+        // i++;
     }
+    // for (i=0; i<2; i++)
+    // {
+    //     // reinstantiate a new Mocha object, run it, then unload files to clear the cache
+        
+    //     mocha = new Mocha();
+    //     console.log("pre mocha run");
+    //     await runMocha();
+    //     // while (itr != i) {
+    //     //     sleep(500);
+    //     // }
+    //     await sleep(5000);
+    //     console.log("post");
+    // }
 }
 
 main();
--- test/testsum.js
+++ test/testsum.js
@@ -2,7 +2,12 @@ var sum = require('../sum');
 var expect = require('chai').expect;
 
 var arr = [2,3]; //n_warmup_iter, n_perf_iter
-
+let sleep = function (time_ms) {
+    let promise = new Promise((resolve) => {
+        setTimeout(() => {resolve();}, time_ms);
+    });
+    return promise;
+}
 // for (var i = 0; i < 2; i++) {
 //     describe('#sum()', function () {
 //         context('without arguments', function () {
@@ -24,16 +29,19 @@ var arr = [2,3]; //n_warmup_iter, n_perf_iter
 
 describe('#sum()', function () {
     context('without arguments', function () {
-        it('should return 0', function() {
+        it('should return 0', async function() {
+            await sleep(1000);
             expect(sum()).to.equal(0);
         })
     })
     context('with number arguments', function() {
-        it('should return sum of arguments', function() {
+        it('should return sum of arguments', async function() {
+            await sleep(1000);
             expect(sum(1,2,3,4,5)).to.equal(15);
         })
 
-        it('should return argument when only 1 argument is passed', function () {
+        it('should return argument when only 1 argument is passed', async function () {
+            await sleep(1000);
             expect(sum(5)).to.equal(5);
         })
     })
